0f335b105788504a80ad17a58d7088d3c6862a2c
IhorrH <85521691+IhorrH@users.noreply.github.com>
2021-06-17 10:07:57 +0300

    Add files via upload

---
+++ ResultsPage.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2021. Created by Ihor Hudyma for TestMatick
+ */
+
+import org.openqa.selenium.WebDriver;
+
+public class ResultsPage {
+    private WebDriver driver;
+
+    public ResultsPage(WebDriver driver)
+    {
+        this.driver = driver;
+    }
+}
